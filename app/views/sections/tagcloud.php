4e2b57e0fd1437bd1fae8a5cdaf54d7c43f7d07e
Ger Hobbelt <ger@hobbelt.com>
2014-04-22 18:09:24 +0200

    fix bug in tagcloud.php which I introduced when changing all short tags to regular <?php tags :-(

--- app/views/sections/tagcloud.php
+++ app/views/sections/tagcloud.php
@@ -1,13 +1,17 @@
 <canvas <?php echo attr("id", $vars['id']); ?> <?php echo attr("class", $vars['class']); ?>>
 <div id="tag-holder" class="hide">
-<?php if(!empty($items)){
-	foreach($items as $item){
-	// excluding empty tags and tags related with organizing content (better logic here...)
-	if( $item['title'] != "" && strpos( $item['title'], "menu-" ) == false && $item['title'] != "category" ){ ?>
-	<a href="<?php echo $item['url']; ?>" class="tag"<?php if($vars['weight']) { echo ' style="font-size: ' . (100 + $item['weight'] * 10) . '%"'; ?>><?php echo $item['title']; ?></a>
-<?php  } ?>
-<?php }
-} ?>
+<?php 
+if (!empty($items)) {
+	foreach ($items as $item) {
+	   // excluding empty tags and tags related with organizing content (better logic here...)
+	   if ( $item['title'] != "" && strpos( $item['title'], "menu-" ) == false && $item['title'] != "category" ) { 
+?>
+	<a href="<?php echo $item['url']; ?>" class="tag"<?php if($vars['weight']) { echo ' style="font-size: ' . (100 + $item['weight'] * 10) . '%"'; } ?>><?php echo $item['title']; ?></a>
+<?php
+        }
+    }
+} 
+?>
 </div>
 </canvas>
 <script type="text/javascript" src="<?php echo url('/js/plugins/jquery.tagcanvas.min.js'); ?>" data-type="require" data-path="tagcanvas" data-deps="jquery"></script>
